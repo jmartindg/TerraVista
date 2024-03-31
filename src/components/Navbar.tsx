52740513b9678c3b6766b4bb77c0945c633965fb
jmartindg <jmartindeguia09@gmail.com>
2024-03-31 16:00:37 +0800

    favicon and logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import Logo from "../assets/image/logo.png";
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-primary">
       <div className="container">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">TerraVista</a>
-        </div>
+        <Link to="/" className="flex-1">
+          <div className="flex items-center space-x-2">
+            <img src={Logo} alt="Logo" className="h-8 w-8" />
+            <p className="font-bold">TerraVista</p>
+          </div>
+        </Link>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1 space-x-2">
             <li>
