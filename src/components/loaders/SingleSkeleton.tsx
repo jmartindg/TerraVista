c4a8c12887796b066663b87f8ea54a16c15ffad5
jmartindg <jmartindeguia09@gmail.com>
2024-04-16 11:03:48 +0800

    react-leaflet added for country and capital map viewing

--- src/components/loaders/SingleSkeleton.tsx
+++ src/components/loaders/SingleSkeleton.tsx
@@ -1,24 +1,31 @@
 const SingleSkeleton = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-      <div>
-        <div className="skeleton aspect-video w-full h-full"></div>
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div>
+          <div className="skeleton aspect-video w-full h-full"></div>
+        </div>
+        <div className="space-y-5">
+          <div className="skeleton h-4 w-1/2"></div>
+          <div className="skeleton h-4 w-1/4"></div>
+          <div className="skeleton h-4 w-full bg-transparent"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-full"></div>
+        </div>
       </div>
-      <div className="space-y-5">
-        <div className="skeleton h-4 w-1/2"></div>
-        <div className="skeleton h-4 w-1/4"></div>
-        <div className="skeleton h-4 w-full bg-transparent"></div>
-        <div className="skeleton h-4 w-full"></div>
-        <div className="skeleton h-4 w-full"></div>
-        <div className="skeleton h-4 w-full"></div>
-        <div className="skeleton h-4 w-full"></div>
-        <div className="skeleton h-4 w-full"></div>
-        <div className="skeleton h-4 w-full"></div>
-        <div className="skeleton h-4 w-full"></div>
-        <div className="skeleton h-4 w-full"></div>
-        <div className="skeleton h-4 w-full"></div>
-      </div>
-    </div>
+
+      <div className="skeleton h-[50vh] md:h-[70vh] w-full mb-12 mt-16"></div>
+
+      <div className="skeleton h-10 pb-3 w-1/4"></div>
+      <div className="skeleton h-[50vh] md:h-[70vh] w-full my-6"></div>
+    </>
   );
 };
 
--- src/pages/country/Country.tsx
+++ src/pages/country/Country.tsx
@@ -3,6 +3,8 @@ import toast from "react-hot-toast";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import SingleSkeleton from "../../components/loaders/SingleSkeleton";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 // Icons
 import { FaUsers, FaMapLocationDot, FaCity, FaLanguage, FaMoneyBill, FaClock, FaCalendarDay } from "react-icons/fa6";
@@ -37,6 +39,8 @@ const Country = () => {
   const [languages, setLanguages] = useState<string>("");
   const [currencies, setCurrencies] = useState<string>("");
   const [timezones, setTimezones] = useState<string>("");
+  const [latlng, setLatlng] = useState<number[]>([]);
+  const [capitalLatlng, setCapitalLatlng] = useState<number[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const { name } = useParams();
 
@@ -63,6 +67,14 @@ const Country = () => {
         const timezonesData = response.data[0].timezones;
         setTimezones(timezonesData.join(", "));
 
+        // Get longitude and latitude
+        const latlng = response.data[0].latlng;
+        setLatlng(latlng);
+
+        // Get capital longitude and latitude
+        const capital = response.data[0].capitalInfo.latlng;
+        setCapitalLatlng(capital);
+
         setTimeout(() => setLoading(false), 1000);
       } catch (error: unknown) {
         toast.error("Oh no! An error occurred." + error);
@@ -78,59 +90,86 @@ const Country = () => {
       {loading ? (
         <SingleSkeleton />
       ) : (
-        <section className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8">
-          <figure>
-            <img src={countries[0]?.flags.svg} className="w-full" alt={countries[0]?.flags.alt} />
-          </figure>
-          <section>
-            <h2 className="text-3xl lg:text-4xl font-bold pb-1">{name}</h2>
-            <p className="font-semibold">
-              Official Name: <span className="font-normal">{countries[0]?.name.official}</span>
-            </p>
-
-            <section className="pt-8 grid grid-cols-1 md:grid-cols-2 gap-5">
-              <p className="font-medium flex items-center">
-                <FaUsers size={24} className="mr-2" />
-                Population:&nbsp;<span className="font-normal">{countries[0]?.population.toLocaleString()}</span>
-              </p>
-              <p className="font-medium flex items-center">
-                <FaMapLocationDot size={24} className="mr-2" />
-                Region:&nbsp;<span className="font-normal">{countries[0]?.region}</span>
-              </p>
-              <p className="font-medium flex items-center">
-                <FaCity size={24} className="mr-2" />
-                Captial:&nbsp;<span className="font-normal">{countries[0]?.capital}</span>
-              </p>
-              <p className="font-medium flex items-center">
-                <FaMapMarkerAlt size={24} className="mr-2" />
-                Subregion:&nbsp;<span className="font-normal">{countries[0]?.subregion}</span>
-              </p>
-              <p className="font-medium flex items-center">
-                <FaCalendarDay size={24} className="mr-2" />
-                Start of Week:&nbsp;<span className="font-normal capitalize">{countries[0]?.startOfWeek}</span>
+        <>
+          <section className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8">
+            <figure>
+              <img src={countries[0]?.flags.svg} className="w-full" alt={countries[0]?.flags.alt} />
+            </figure>
+            <section>
+              <h2 className="text-3xl lg:text-4xl font-bold pb-1">{name}</h2>
+              <p className="font-semibold">
+                Official Name: <span className="font-normal">{countries[0]?.name.official}</span>
               </p>
 
-              <p className="font-medium flex items-center">
-                <FaMoneyBill size={24} className="mr-2" />
-                {currencies.length < 2 ? "Currencies" : "Currency"}:&nbsp;
-                <span className="font-normal capitalize">{currencies}</span>
-              </p>
-            </section>
+              <section className="pt-8 grid grid-cols-1 md:grid-cols-2 gap-5">
+                <p className="font-medium flex items-center">
+                  <FaUsers size={24} className="mr-2" />
+                  Population:&nbsp;<span className="font-normal">{countries[0]?.population.toLocaleString()}</span>
+                </p>
+                <p className="font-medium flex items-center">
+                  <FaMapLocationDot size={24} className="mr-2" />
+                  Region:&nbsp;<span className="font-normal">{countries[0]?.region}</span>
+                </p>
+                <p className="font-medium flex items-center">
+                  <FaCity size={24} className="mr-2" />
+                  Captial:&nbsp;<span className="font-normal">{countries[0]?.capital}</span>
+                </p>
+                <p className="font-medium flex items-center">
+                  <FaMapMarkerAlt size={24} className="mr-2" />
+                  Subregion:&nbsp;<span className="font-normal">{countries[0]?.subregion}</span>
+                </p>
+                <p className="font-medium flex items-center">
+                  <FaCalendarDay size={24} className="mr-2" />
+                  Start of Week:&nbsp;<span className="font-normal capitalize">{countries[0]?.startOfWeek}</span>
+                </p>
 
-            <section className="pt-5 space-y-5">
-              <p className="font-medium flex items-center">
-                <FaLanguage size={24} className="mr-2" />
-                {languages.length < 2 ? "Language" : "Languages"}:&nbsp;
-                <span className="font-normal">{languages}</span>
-              </p>
-              <p className="font-medium flex items-center">
-                <FaClock size={24} className="mr-2" />
-                {timezones.length < 2 ? "Timezone" : "Timezones"}:&nbsp;
-                <span className="font-normal capitalize">{timezones}</span>
-              </p>
+                <p className="font-medium flex items-center">
+                  <FaMoneyBill size={24} className="mr-2" />
+                  {currencies.length < 2 ? "Currencies" : "Currency"}:&nbsp;
+                  <span className="font-normal capitalize">{currencies}</span>
+                </p>
+              </section>
+
+              <section className="pt-5 space-y-5">
+                <p className="font-medium flex items-center">
+                  <FaLanguage size={24} className="mr-2" />
+                  {languages.length < 2 ? "Language" : "Languages"}:&nbsp;
+                  <span className="font-normal">{languages}</span>
+                </p>
+                <p className="font-medium flex items-center">
+                  <FaClock size={24} className="mr-2" />
+                  {timezones.length < 2 ? "Timezone" : "Timezones"}:&nbsp;
+                  <span className="font-normal capitalize">{timezones}</span>
+                </p>
+              </section>
             </section>
           </section>
-        </section>
+
+          <section className="mb-12 mt-16">
+            <MapContainer center={[latlng[0], latlng[1]]} zoom={5} scrollWheelZoom={false}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[latlng[0], latlng[1]]}>
+                <Popup>{countries[0]?.name.official}</Popup>
+              </Marker>
+            </MapContainer>
+          </section>
+
+          <section className="my-6">
+            <h2 className="text-xl md:text-2xl pb-3 font-semibold">Capital Map: {countries[0]?.capital}</h2>
+            <MapContainer center={[capitalLatlng[0], capitalLatlng[1]]} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[capitalLatlng[0], capitalLatlng[1]]}>
+                <Popup>{countries[0]?.capital}</Popup>
+              </Marker>
+            </MapContainer>
+          </section>
+        </>
       )}
     </section>
   );
